75d8e96a124916d38193ef2e8dbab0077e5b2c6e
Gregory Voss <gvoss@tflow.net>
2016-04-24 09:28:57 -0500

    pushing robottest

---
+++ Robot Test/Epson_SPEL.vb
@@ -0,0 +1,15 @@
+﻿Module Epson_SPEL
+    Public WithEvents Scara As RCAPINet.Spel
+    Public Sub InitApp()
+        Scara = New RCAPINet.Spel
+        With Scara
+            .Initialize()
+            .Project = "c:\EpsonRC70\projects\API_Demos\Demo1 \demo1.sprj"
+        End With
+    End Sub
+
+    Public Sub EventReceived(ByVal sender As Object, ByVal e As RCAPINet.SpelEventArgs) Handles Scara.EventReceived
+        MsgBox("received event " & e.Event)
+    End Sub
+
+End Module
---
+++ Robot Test/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        Scara.Dispose()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Epson_SPEL.InitApp()
+
+        Scara.AvoidSingularity = True
+        If Not Scara.MotorsOn Then
+            Scara.MotorsOn = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Scara.Stop()
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Btn_Pause.Enabled = True
+        Button2.Enabled = False
+        Dim xcord As Single
+        Dim ycord As Single
+        Dim zcord As Single
+        xcord = CSng(TextBox1.Text)
+        ycord = CSng(TextBox2.Text)
+        zcord = CSng(TextBox3.Text)
+
+        Btn_Continue.Enabled = False
+        Scara.SetPoint(1, 200, 200, -70, 0)
+        Scara.SetPoint(2, xcord, ycord, zcord, 0)
+
+        Scara.Jump(1)
+        Scara.Jump(2)
+
+
+    End Sub
+
+    Private Sub Btn_Pause_Click(sender As Object, e As EventArgs) Handles Btn_Pause.Click
+        Scara.Pause()
+        Btn_Pause.Enabled = False
+        Button2.Enabled = True
+        Btn_Continue.Enabled = True
+
+    End Sub
+
+    Private Sub Btn_Continue_Click(sender As Object, e As EventArgs) Handles Btn_Continue.Click
+        Scara.Continue()
+        Btn_Pause.Enabled = True
+        Btn_Continue.Enabled = False
+    End Sub
+End Class
