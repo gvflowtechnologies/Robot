07c1faeec3cdc822b051de36c2bbafc826d0a4ff
gvflowtechnologies <greg.voss64@gmail.com>
2016-04-27 16:10:02 -0500

    Added inital vacuum

--- Robot Test/Form1.vb
+++ Robot Test/Form1.vb
@@ -9,14 +9,14 @@ Public Class Form1
     Dim Front1() As Single = {260.733, 302.082, -111.144, -148.46}
     Public Inside1() As Single = {57.937, 184.83, -111.334, -178.046}
     Public Outside1() As Single = {391.447, 76.419, -110.722, -185.602}
-    Public rows As Integer = 20
-    Public columns As Integer = 20
+    Public rows As Integer = 19
+    Public columns As Integer = 21
     Dim startx As Single
     Dim starty As Single
 
     Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
         Scara.Dispose()
-
+        Scara.Out(1, 0)
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -25,7 +25,7 @@ Public Class Form1
         If Not Scara.MotorsOn Then
             Scara.MotorsOn = True
         End If
-        Scara.SetPoint(scalepoint, -2.1, 268.4, -70, -175, 0, RCAPINet.SpelHand.Lefty)
+        Scara.SetPoint(scalepoint, -2.1, 268.4, -100, 0, 0, RCAPINet.SpelHand.Lefty)
 
 
     End Sub
@@ -40,34 +40,83 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Btn_Pause.Enabled = True
         Button2.Enabled = False
-        Scara.PowerHigh = False
+        Scara.PowerHigh = True
         Dim xcord As Single
         Dim ycord As Single
         Dim zcord As Single
-        xcord = CSng(TextBox1.Text)
-        ycord = CSng(TextBox2.Text)
-        zcord = CSng(TextBox3.Text)
-        Scara.Tool(1)
+        Dim xinc1 As Single
+        Dim yinc1 As Single
+        Dim xinc2 As Single
+        Dim yinc2 As Single
 
+        xinc1 = (Back1(0) - Inside1(0)) / (columns - 1)
+        yinc1 = (Back1(1) - Inside1(1)) / (columns - 1)
+        xinc2 = (Back1(0) - Outside1(0)) / (rows - 1)
+        yinc2 = (Back1(1) - Outside1(1)) / (rows - 1)
+
+
+
+
+        'xcord = CSng(TextBox1.Text)
+        'ycord = CSng(TextBox2.Text)
+        'zcord = CSng(TextBox3.Text)
+
+        Scara.Tool(1)
+        Scara.LimZ(-65)
+        Scara.Speed(60)
+        Scara.Accel(30, 30)
         Btn_Continue.Enabled = False
 
-        Scara.SetPoint(1, -2.1, 268.4, -70, -175, 0, RCAPINet.SpelHand.Lefty)
+        xcord = Back1(0)
+        ycord = Back1(1)
+        zcord = Back1(2) + 5
+        Dim ucord As Single
+
+        Scara.SetPoint(1, xcord, ycord, zcord, -175, 0, RCAPINet.SpelHand.Lefty)
         Dim r As Integer
         Dim c As Integer
 
-        Dim count As Integer
-        For r = 0 To 
-            For c = 1 To 20
+        ucord = 0
+        '      Scara.PowerHigh = True
+        For r = 0 To rows - 1
+            If r > 10 Then ucord = -180
+            For c = 0 To columns - 1
+
+                xcord = Back1(0) - c * xinc1 - r * xinc2
+                ycord = Back1(1) - c * yinc1 - r * yinc2
 
 
-                Scara.SetPoint(2, xcord, ycord, zcord, -181.0, 0, RCAPINet.SpelHand.Lefty)
+
+                Scara.SetPoint(1, xcord, ycord, zcord, ucord, 0, RCAPINet.SpelHand.Lefty)
+
+                ' Scara.SetPoint(2, xcord, ycord, zcord, -181.0, 0, RCAPINet.SpelHand.Lefty)
+
+                Scara.Jump(1)
+                
+                Scara.SetPoint(1, xcord, ycord, zcord + 10, ucord, 0, RCAPINet.SpelHand.Lefty)
+                Scara.Jump(1)
+                Scara.Out(1, 1)
+                Scara.Delay(1000)
+
+
+                Scara.SetPoint(1, xcord, ycord, zcord, ucord, 0, RCAPINet.SpelHand.Lefty)
+
+                Scara.Jump(1)
+                Scara.Out(1, 2)
+                Scara.Delay(250)
+
+
 
                 Scara.Jump(scalepoint)
-
+                'Scara.Out(1, 1)
+                'Scara.Delay(250)
+                'Scara.Out(1, 2)
+                'Scara.Delay(250)
 
             Next
-        Next
 
+        Next
+        Scara.Out(1, 0)
 
     End Sub
 
