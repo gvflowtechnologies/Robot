428574b7dc62857bb9cb5d51239ef99a0dfc7739
Gregory Voss <gvoss@tflow.net>
2016-05-05 20:23:27 -0500

    Added Door

--- Robot Test/Form1.Designer.vb
+++ Robot Test/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Btn_Pause = New System.Windows.Forms.Button()
@@ -32,7 +33,7 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
         'Button1
@@ -122,21 +123,14 @@ Partial Class Form1
         Me.Label3.TabIndex = 9
         Me.Label3.Text = "Z"
         '
-        'Button3
+        'Timer1
         '
-        Me.Button3.Location = New System.Drawing.Point(36, 147)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 10
-        Me.Button3.Text = "Motor"
-        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -163,6 +157,6 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
 
 End Class
--- Robot Test/Form1.vb
+++ Robot Test/Form1.vb
@@ -1,10 +1,12 @@
 ﻿Imports RCAPINet
+Imports System.Threading
 
 Public Class Form1
 
     Const scalepoint As Integer = 10
     Const goodpoint1 As Integer = 11
     Const badpoint1 As Integer = 12
+    Const pausepoint As Integer = 13
     Dim Back1() As Single = {187.804, -40.051, -111.231, -262.7}
     Dim Front1() As Single = {260.733, 302.082, -111.144, -148.46}
     Public Inside1() As Single = {57.937, 184.83, -111.334, -178.046}
@@ -13,20 +15,26 @@ Public Class Form1
     Public columns As Integer = 21
     Dim startx As Single
     Dim starty As Single
+    Dim PauseRequest As Boolean
+    Dim ResumeMotion As Boolean
 
     Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
-        Scara.Dispose()
+        Scara.Stop()
         Scara.Out(1, 0)
+        Scara.Dispose()
+
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Epson_SPEL.InitApp()
-
+        PauseRequest = False
+        ResumeMotion = False
+        Btn_Continue.Enabled = False
         If Not Scara.MotorsOn Then
             Scara.MotorsOn = True
         End If
         Scara.SetPoint(scalepoint, -2.1, 268.4, -100, 0, 0, RCAPINet.SpelHand.Lefty)
-
+        Scara.SetPoint(pausepoint, 100, 100, -70, 0, 0, RCAPINet.SpelHand.Righty)
 
     End Sub
 
@@ -87,32 +95,34 @@ Public Class Form1
 
 
 
-                Scara.SetPoint(1, xcord, ycord, zcord, ucord, 0, RCAPINet.SpelHand.Lefty)
+                '   Scara.SetPoint(1, xcord, ycord, zcord, ucord, 0, RCAPINet.SpelHand.Lefty)
 
                 ' Scara.SetPoint(2, xcord, ycord, zcord, -181.0, 0, RCAPINet.SpelHand.Lefty)
 
-                Scara.Jump(1)
-                
+                '        If Scara.In(1) = 0 Then
+
+                'Scara.Jump(1)
+                ' Spit out one
                 Scara.SetPoint(1, xcord, ycord, zcord + 10, ucord, 0, RCAPINet.SpelHand.Lefty)
                 Scara.Jump(1)
                 Scara.Out(1, 1)
                 Scara.Delay(1000)
-
-
+                If PauseRequest = True Then Controlled_Pause()
+                ' Pick up one
                 Scara.SetPoint(1, xcord, ycord, zcord, ucord, 0, RCAPINet.SpelHand.Lefty)
 
                 Scara.Jump(1)
                 Scara.Out(1, 2)
                 Scara.Delay(250)
 
-
+                If PauseRequest = True Then Controlled_Pause()
 
                 Scara.Jump(scalepoint)
                 'Scara.Out(1, 1)
                 'Scara.Delay(250)
                 'Scara.Out(1, 2)
                 'Scara.Delay(250)
-
+                If PauseRequest = True Then Controlled_Pause()
             Next
 
         Next
@@ -121,25 +131,64 @@ Public Class Form1
     End Sub
 
     Private Sub Btn_Pause_Click(sender As Object, e As EventArgs) Handles Btn_Pause.Click
-        Scara.Pause()
+        ' Set flag to request pause
+        PauseRequest = True
+        Me.BackColor = Color.IndianRed
+        ResumeMotion = False
         Btn_Pause.Enabled = False
-        Button2.Enabled = True
         Btn_Continue.Enabled = True
-
     End Sub
 
     Private Sub Btn_Continue_Click(sender As Object, e As EventArgs) Handles Btn_Continue.Click
-        Scara.Continue()
+        ResumeMotion = True
+    End Sub
+    Private Sub Controlled_Pause()
+
+        ' 1. Jump to location.
+        ' 2. Enable button to start
+        ' 3. Wait for continue to be pressed
+
+
+        Scara.Jump(pausepoint)
+
+        Do
+            Application.DoEvents()
+            Thread.Sleep(1)
+
+        Loop Until ResumeMotion = True
+
         Btn_Pause.Enabled = True
         Btn_Continue.Enabled = False
-    End Sub
+        PauseRequest = False
+        Me.BackColor = Color.BurlyWood
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If Not Scara.MotorsOn Then
-            Scara.MotorsOn = True
-        Else
-            Scara.MotorsOn = False
+    End Sub
+ 
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Check if door is open, if the door is open then pause the robot if not already paused.
+        ' If door is closesd, then have the robot conitnue if not already running.
+
+        If Scara.In(2) = 8 Or Scara.In(2) = 9 Then 'Robot should be running
+            ' Check if robot is paused or not
+
+            If Scara.PauseOn = True Then
+                ' If paused is on turn on robot
+                ' If make no change
+                Scara.Continue()
+            End If
+
+        Else ' Robot should not be running
+            ' Check to see if robot is paused or not.
+
+            If Scara.PauseOn = False Then
+                ' If the robot is not paused, then pause it.
+                ' Otherwise make no change
+                Scara.Pause()
+
+            End If
+
         End If
-    End Sub
 
+    End Sub
 End Class
